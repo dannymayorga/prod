d9f03d0636d3456370be7de81d2f67821bd22076
Danny Mayorga <dannymayorga81@gmail.com>
2023-03-29 17:30:18 -0400

    Update error  on script.js

--- public/script.js
+++ public/script.js
@@ -25,11 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const shortForm = document.querySelector("#short-form").value;
 
         try {
-            // Replace the URL with the Heroku URL where your server-side code is hosted
             const response = await fetch(`https://prod-ai-1.herokuapp.com/generate-content?type=${selectedType}&longForm=${encodeURIComponent(longForm)}&shortForm=${encodeURIComponent(shortForm)}`);
 
             const data = await response.json();
-            if (data && data.choices && data.choices.length > 0) {
+            if (data.error) {
+                handleError(`Error: ${data.error.message}`);
+            } else if (data.choices && data.choices.length > 0) {
                 const aiGeneratedContent = data.choices[0].text;
                 outputPane.innerHTML = aiGeneratedContent;
             } else {
