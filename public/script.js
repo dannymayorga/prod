868786c613e23be58757c4b2911ffa358edbfc80
Danny Mayorga <dannymayorga81@gmail.com>
2023-03-29 21:59:15 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const role = document.querySelector("#role").value;
         const title = document.querySelector(`#${selectedType}-title`).value;
-        const description = document.querySelector(`#${selectedType}-description`).value; // Corrected line
+        const description = document.querySelector(`#${selectedType}-description`).value; // Corrected ID
 
         try {
             const response = await fetch(`https://prod-ai-1.herokuapp.com/generate-content?type=${selectedType}&role=${encodeURIComponent(role)}&title=${encodeURIComponent(title)}&description=${encodeURIComponent(description)}`);
