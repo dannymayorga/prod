501a4b318119979ef106274357665ea6e85a7517
Danny Mayorga <dannymayorga81@gmail.com>
2023-03-31 01:03:46 -0400

    Update error  on index.html

--- app.js
+++ app.js
@@ -1,67 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/generate-content", async (req, res) => {
-  // Extract the values of the new fields from the request query parameters
-  const { type, role, title, description } = req.query;
-  const apiKey = process.env.CHATGPT_API_KEY;
-
-  let prompt = "";
-  switch (type) {
-    case "epic":
-      prompt = `As a ${role}, I want to create an Epic with the title "${title}" and the following description: ${description}\n\n`;
-      break;
-    case "feature":
-      prompt = `As a ${role}, I want to create a Feature with the title "${title}" and the following description: ${description}\n\n`;
-      break;
-    case "user-story":
-      prompt = `As a ${role}, I want to create User Stories with the title "${title}" and the following description: ${description}\n\n`;
-      break;
-    case "editor":
-      prompt = `${description}\n\n`;
-      break;
-    default:
-      return res.status(400).send("Invalid content type.");
-  }
+app.post('/generate-content', async (req, res) => {
+  const prompt = req.body.prompt;
 
   try {
-    const nodeFetch = await import('node-fetch');
-    const fetch = nodeFetch.default;
+    // Replace with your ChatGPT API URL
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+    // Use the environment variable for the API key
+    const apiKey = process.env.CHATGPT_API_KEY;
 
-    const response = await fetch("https://api.openai.com/v1/engines/davinci/completions", {
-      method: "POST",
+    const response = await axios.post(apiUrl, { prompt: prompt }, {
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 100,
-        n: 3, // Generate 3 different options
-        stop: null,
-        temperature: 1,
-      }),
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      }
     });
 
-    const data = await response.json();
-    res.json(data);
+    // Send the generated content back to the client
+    res.json(response.data);
   } catch (error) {
-    console.error("Error fetching ChatGPT:", error);
-    res.status(500).send("Error: Unable to generate content.");
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while generating content.' });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
